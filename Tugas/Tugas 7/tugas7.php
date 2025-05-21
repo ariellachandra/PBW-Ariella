b37a14f15002f8cad0a93843adb9cfe682ef6aa6
Ariella Chandra Naya <chandranaya02@gmail.com>
2025-05-21 23:40:58 +0700

    Menambahkan Tugas dan Praktikum

--- Tugas/Tugas 7/tugas7.php
+++ Tugas/Tugas 7/tugas7.php
@@ -7,26 +7,40 @@
 </head>
 <body>
 
-<h2>Latihan Praktikum</h2>
+
+
+<?php if (!isset($_GET['soal'])): ?>
+    <h2>Latihan Praktikum</h2>
+<nav>
+    <ul>
+        <li><a href="?soal=1">Nomor 1</a></li>
+        <li><a href="?soal=2">Nomor 2</a></li>
+        <li><a href="?soal=3">Nomor 3</a></li>
+        <li><a href="?soal=4">Nomor 4</a></li>
+    </ul>
+</nav>
+<?php endif; ?>
 
 <?php
+if (isset($_GET['soal'])) {
+    $soal = $_GET['soal'];
 
-    // include 'no1.php';
-    // include 'no2.php';
-    // include 'no3.php';
-    // include 'no4.php';
+    if ($soal == '1') {
+        include 'no1.php';
+    } elseif ($soal == '2') {
+        include 'no2.php';
+    } elseif ($soal == '3') {
+        include 'no3.php';
+    } elseif ($soal == '4') {
+        include 'no4.php';
+    } else {
+        echo "<p>Soal tidak ditemukan.</p>";
+    }
 
-    echo '<a href="no1.php">Nomor 1</a>';
-    echo "<br>";
-    echo '<a href="no2.php">Nomor 2</a>';
-    echo "<br>";
-    echo '<a href="no3.php">Nomor 3</a>';
-    echo "<br>";
-    echo '<a href="no4.php">Nomor 4</a>';
-
-
+} else {
+    echo "<p><h2>Sekian Terima Kasih</h2></p>";
+}
 ?>
 
-<h2>Sekian Terima Kasih</h2>
 </body>
 </html>
